a89d64295e3ca8ef967358e93c6565ff7fb02789
DoinaGanceriuc <doinaganceriuc@gmail.com>
2021-12-09 17:26:07 +0100

    add bonus

---
+++ bonus/index.php
@@ -0,0 +1,13 @@
+<?php
+require __DIR__ . '/layouts/head.php';
+
+?>
+
+<?php require __DIR__ . '/layouts/header.php';?>
+
+    <main id="site_main">
+        <?php require __DIR__ . '/partials/filtered.php';?>
+
+    </main>
+    <!-- /#site_main -->
+ <?php require __DIR__ . '/layouts/footer.php';?>
